bff1a0806065be5a84f0eaa52d6428036e2faf39
Melissa Nguyen <nguyen.melissa23@gmail.com>
2017-04-21 23:09:11 -0700

    lambda function for git pull

---
+++ skill/src/index.js
@@ -0,0 +1,5 @@
+const AlexaLambdaHandler = require('./lib/alexa');
+
+module.exports.handler = AlexaLambdaHandler.LambdaHandler; 
+module.exports.CreateStateHandler = AlexaLambdaHandler.CreateStateHandler;
+module.exports.StateString = AlexaLambdaHandler.StateString;
---
+++ skill/src/lib/alexa.js
@@ -0,0 +1,12 @@
+'Use strict';
+
+const EventEmitter = require('event').EventEmitter;
+const util = require('until');
+const i18n = require('i18next');
+const sprintf = require('./DynomoAttributesHalper');
+const responseHandlers = require('./response');
+const _StateString = 'STATE';
+
+function AlexaRequestEmitter(){
+    
+}

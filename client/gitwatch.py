ae06d73e171aabf516e01b6ddacc97853c354ef2
ThisChessPlayer <jasonma5501@gmail.com>
2017-04-23 16:39:51 -0700

    added line endings so commands properly terminate, also added push

--- client/gitwatch.py
+++ client/gitwatch.py
@@ -26,8 +26,9 @@ MESSAGE = "added line endings so commands properly terminate, also added push"
 def git_pull():
 
   os.chdir(DIR)
-  os.popen("git pull origin master\n")
-  time.sleep(2)
+  #os.popen("git pull origin master\n")
+  os.system("git pull origin master\n")
+  #os.wait()
 
 '''[git_commit]----------------------------------------------------------------
 
@@ -35,8 +36,8 @@ def git_pull():
 def git_commit():
 
   os.chdir(DIR)
-  os.popen("git commit -m \"" + MESSAGE + "\"\n")
-  time.sleep(2)
+  os.system("git commit -m \"" + MESSAGE + "\"\n")
+  time.sleep(5)
 
 '''[git_push]------------------------------------------------------------------
 
@@ -44,8 +45,8 @@ def git_commit():
 def git_push():
 
   os.chdir(DIR)
-  os.popen("git push origin master\n")
-  time.sleep(2)
+  os.system("git push origin master\n")
+  time.sleep(5)
 
 parser = argparse.ArgumentParser(description = 'Convert commands into git commands')
 parser.add_argument('-d', nargs=1, help='directory of repo')
@@ -58,8 +59,8 @@ args = parser.parse_args()
 if args.pull:
   git_pull()
 
-#if args.push:
-#  git_push()
+if args.push:
+  git_push()
 
 if args.commit:
   git_commit()

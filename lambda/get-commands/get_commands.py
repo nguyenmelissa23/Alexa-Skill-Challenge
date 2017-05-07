b6e9a6fb0ec0ed0f9fe33441070c25c64c6764e5
Graham McKnight <graham.mcknight2@gmail.com>
2017-05-06 17:21:45 -0700

    Tested all methods

--- lambda/get-commands/get_commands.py
+++ lambda/get-commands/get_commands.py
@@ -5,7 +5,6 @@ import pymysql
 import constants
 import random
 import string
-from passlib.hash import pbkdf2_sha256
 
 #rds settings
 rds_host  = rds_config.db_host
@@ -26,24 +25,24 @@ def handler(event, context):
     commandlist = []
 
     with conn.cursor() as cursor:
-        cursor.execute("SELECT * FROM `client_device` WHERE `token` = \"{0}\";".format(device_token))
+        cursor.execute("SELECT * FROM `client_device` WHERE `device_token` = \"{0}\";".format(device_token))
         result = cursor.fetchall()
         if len(result) == 1:
             device_id = result[0][constants.ID_INDEX]
-            cursor.execute("SELECT `device_commands`.`command_id`, `device_commands`.`command_extra`, `user_repository`.`path`" +
-                           " FROM `device_commands` " +
-                           " WHERE `device_id` = \"{0}\"" .format(device_id)) +
-                           " INNER JOIN `user_repository` ON `device_commands`.`repo_id` = `user_repository`.`id`);")
+            cursor.execute("SELECT `device_command`.`command_id`, `device_command`.`command_extra`, `user_repository`.`path`" +
+                           " FROM `device_command`" +
+                           " INNER JOIN `user_repository` ON `device_command`.`repo_id` = `user_repository`.`id`" +
+                           " WHERE `device_command`.`device_id` = \"{0}\";".format(device_id))
             
             result = cursor.fetchall()
             for command in result:
                 commandlist.append({
                     'command_id' : command[constants.COMMAND_ID_INDEX],
                     'command_extra' : command[constants.COMMAND_EXTRA_INDEX],
-                    'path' : command[COMMAND_PATH_INDEX]
+                    'path' : command[constants.COMMAND_PATH_INDEX]
                 })
-        else:
-            result = []
+            cursor.execute("DELETE FROM `device_command` WHERE `device_id` = {0};".format(device_id))
+            
         conn.commit()
         conn.close()
         return [commandlist]

160f3b38d65ad519b5cc618ba98a21ad81930fbb
nguyenmelissa23 <nguyen.melissa23@gmail.com>
2017-05-07 13:37:45 -0700

    webpages with private policy

--- public/assets/js/logic.js
+++ public/assets/js/logic.js
@@ -1,29 +1,54 @@
 //This is to register:
-$("#submitRegister").on("click", function(){
-    $.ajax({
-            url: "https://rmkw7wi6z9.execute-api.us-west-1.amazonaws.com/Production/users", //api url
-            method: "POST",
-            data: {
-                username: $("#registerUsername"),
-                password: $("#registerPassword")
-            }})
-        .done(function(msg){
-            console.log(msg);
+$("#submitRegister").on("click", function(event){
+    event.preventDefault();
+    var queryURL =  "https://rmkw7wi6z9.execute-api.us-west-1.amazonaws.com/Production/users";
+    var userInfoObj = {
+                        "username": $("#registerUsername").val(),
+                        "password": $("#registerPassword").val()
+                    };
+
+    console.log("queryURL", queryURL);
+    console.log("userInfoObj", userInfoObj);
+
+
+    var request = $.post(queryURL, JSON.stringify(userInfoObj));
+
+    request.done(function(data){
+        console.log("request.done");
+        console.log(data);
+    });
+
+    request.fail(function(data){
+        console.log("request.fail");
+        console.log(data);
     });
 });
 
     
 
 //This is to login:
-// $.ajax({
-//         url: "https://rmkw7wi6z9.execute-api.us-west-1.amazonaws.com/Production/users", //api url
-//         method: "POST",
-//         data: {
-//             username: $("#createUsername"),
-//             password: $("#createPassword")
-//         }
-//         })
-//     .done(function(msg){
-//         console.log(msg);
-// });
+$("#submitLogin").on("click", function(event){
+    event.preventDefault();
+    var queryURL =  "https://rmkw7wi6z9.execute-api.us-west-1.amazonaws.com/Production/alexa";
+    var userInfoObj = {
+                        "username": $("#loginUsername").val(),
+                        "password": $("#loginPassword").val()
+                    };
 
+    console.log("queryURL", queryURL);
+    console.log("userInfoObj", userInfoObj);
+
+
+    var request = $.post(queryURL, JSON.stringify(userInfoObj));
+
+    request.done(function(data){
+        console.log("request.done");
+        console.log(data);
+    });
+
+    request.fail(function(data){
+        console.log("request.fail");
+        console.log(data);
+        var alexaQueryString = ""
+    });
+});
